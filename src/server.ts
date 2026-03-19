704d7bbd5b17cf49a7a5e71d4590178afceb79f7
Sergio Guthyerres dos Santos de Morais <hyp@Sergios-MacBook-Air.local>
2026-03-19 12:08:07 -0300

    Instalação e configuração inicial do fastify

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({ logger: true });
+
+fastify.get("/", async (request, reply) => {
+  return { status: "Backend do CodeSurv operante!" };
+});
+// depois irei refatorar para encaixar o socket.io
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    console.log("Servidor rodando na porta 3000");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
